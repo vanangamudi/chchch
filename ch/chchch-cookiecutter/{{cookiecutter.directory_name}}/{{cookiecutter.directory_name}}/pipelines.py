bfefa129295298e0ef502935d92e9adf08cf6f5e
vanangamudi <selva.developer@gmail.com>
2021-06-10 11:47:51 +0530

    cookiecutter template for chchch

---
+++ ch/chchch-cookiecutter/{{cookiecutter.directory_name}}/{{cookiecutter.directory_name}}/pipelines.py
@@ -0,0 +1,55 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+import os
+import re
+import json
+import datetime
+
+import pymongo
+
+from {{cookiecutter.directory_name}}.settings import DATA_DIR
+from {{cookiecutter.directory_name}}.settings import (MONGODB_COLLECTION, MONGODB_DB,
+                             MONGODB_SERVER, MONGODB_PORT)
+
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            MONGODB_SERVER,
+            MONGODB_PORT
+        )
+        db = connection[MONGODB_DB]
+        self.collection = db[MONGODB_COLLECTION]
+
+    def process_item(self, item, spider):
+        for data in item:
+            if not data:
+                raise DropItem("Missing data!")
+
+        
+        retval = self.collection.replace_one(
+            {'url' : item['url'] },
+            dict(item),
+            upsert = True
+        )
+        
+        return item
+
+
+    
+class Pipeline(MongoDBPipeline):
+    def __init__(self):
+        super().__init__()
+        
+    def process_item(self, item, spider):
+
+        return super().process_item(item, spider)
+    
+    
---
+++ ch/chchch-cookiecutter/{{cookiecutter.directory_name}}/{{cookiecutter.directory_name}}/spiders/{{cookiecutter.directory_name}}.py
@@ -0,0 +1,105 @@
+import re
+import bs4
+import pdb
+import datetime
+import urllib
+from pprint import pprint, pformat
+from w3lib.html import remove_tags, remove_tags_with_content
+
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from {{cookiecutter.directory_name}}.items import Item
+
+from  dateutil.parser import parse as fromisoformat
+
+class {{cookiecutter.directory_name}}Spider(CrawlSpider):
+    name = '{{cookiecutter.directory_name}}_spider'
+    allowed_domains = ['{{cookiecutter.url}}']
+    start_urls = ['http://{{cookiecutter.url}}']
+
+    rules = [
+        
+        Rule(
+            LinkExtractor(
+                allow=[r'\w+/\w+/\w+'],
+
+            ),
+            callback='parse_news',
+            follow=True,
+        ),
+
+    ]
+
+    errored_count = 0
+
+    def _make_date(self, date_string):
+        return fromisoformat(date_string)
+        
+    def parse_news(self, response):
+
+        if self.errored_count and self.errored_count % 20 == 0:
+            print('errored_count:{}'.format(self.errored_count))
+            pprint(self.crawler.stats.get_stats())
+
+        try:
+            selector     = response.xpath(
+                '//article'
+            )
+            
+		
+		
+            if not selector:
+                return
+            
+            item = Item()
+
+            item['url']        = response.url                                                      
+            item['filename']   = response.url.split('/')[-1].split('?')[0]                         
+            item['breadcrumb'] = response.xpath(
+                '//h2[@id="system-breadcrumb"]/ol[@class="breadcrumb"]/li/a/text()'
+            ).extract()
+            
+            author = selector.xpath(
+                '//span[contains(@class,"contributor-name")]/text()'
+            )
+
+            if author:
+                item['author'] = [i.extract().strip() for i in author]
+            else:
+                item['author'] = []
+            
+            item['date']    = selector.xpath(
+                '//span[contains(@class,"published")]/time/@datetime'
+            ).extract()[0].strip()
+
+            item['date']    =  self._make_date(item['date'])
+
+            item['content'] = selector.xpath(
+                '//div[contains(@class, "story-element-text")]'
+            ).extract()
+            
+            item['content'] = '\n\n'.join([
+                remove_tags(remove_tags_with_content(content, ('script', ))).strip()
+                for content in item['content']
+            ])
+            
+            item['title']   = selector.xpath(
+                '//h1[contains(@class,"headline")]/text()'
+            )[0].extract().strip()
+            
+            item['tags']    = selector.xpath(
+                '//ul[contains(@class, "tags")]//a/text()'
+            ).extract()
+
+            
+            if item['content']:
+                yield item
+
+        except KeyboardInterrupt:
+            print('got killed by the keyboard :(')
+            raise KeyboardInterrupt
+        except:
+            self.errored_count += 1
+            self.logger.exception(urllib.parse.unquote(response.url))
+            
